c0db3b5abb471b0cb7f7ebbba10931af8b679367
theo <me@t3.gg>
2021-10-24 00:01:41 -0700

    Working kind of for gen 6

--- mod.ts
+++ mod.ts
@@ -1,1 +1,9 @@
 export {};
+
+import PkBase from "./structures/pkbase.ts";
+
+const data = await Deno.readFile("./data/venus.pk6");
+
+const eevee = new PkBase(data, 0, 3, false);
+
+console.log("eevee?", eevee);
